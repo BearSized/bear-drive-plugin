6627f3e00eaa631ef62d92b1b19d914cbb3476b4
BearSized <Styles@Bearsized.com>
2025-05-27 22:38:45 -0400

    Update googleDocsSheets.js

--- googleDocsSheets.js
+++ googleDocsSheets.js
@@ -1,14 +1,13 @@
 const { google } = require('googleapis');
 const auth = require('./auth');
 
-// ✅ Create a new Google Doc
+// Google Docs
 async function createGoogleDoc(title) {
   const docs = google.docs({ version: 'v1', auth });
   const res = await docs.documents.create({ requestBody: { title } });
   return res.data;
 }
 
-// ✅ Write content to a Google Doc
 async function writeToDoc(docId, content) {
   const docs = google.docs({ version: 'v1', auth });
   await docs.documents.batchUpdate({
@@ -19,15 +18,14 @@ async function writeToDoc(docId, content) {
   });
 }
 
-// ✅ Export a Google Doc (as plain text or other supported formats)
 async function exportGoogleDoc(fileId, mimeType = 'text/plain') {
   const client = await auth.getClient();
   const drive = google.drive({ version: 'v3', auth: client });
 
-  const res = await drive.files.export(
-    { fileId, mimeType },
-    { responseType: 'stream' }
-  );
+  const res = await drive.files.export({
+    fileId,
+    mimeType
+  }, { responseType: 'stream' });
 
   return new Promise((resolve, reject) => {
     let data = '';
@@ -37,16 +35,13 @@ async function exportGoogleDoc(fileId, mimeType = 'text/plain') {
   });
 }
 
-// ✅ Create a new Google Sheet
+// Google Sheets
 async function createGoogleSheet(title) {
   const sheets = google.sheets({ version: 'v4', auth });
-  const res = await sheets.spreadsheets.create({
-    requestBody: { properties: { title } }
-  });
+  const res = await sheets.spreadsheets.create({ requestBody: { properties: { title } } });
   return res.data;
 }
 
-// ✅ Append data to a Google Sheet
 async function appendToSheet(sheetId, range, values) {
   const sheets = google.sheets({ version: 'v4', auth });
   await sheets.spreadsheets.values.append({
@@ -57,7 +52,6 @@ async function appendToSheet(sheetId, range, values) {
   });
 }
 
-// ✅ Export all functions
 module.exports = {
   createGoogleDoc,
   writeToDoc,

dfac1f9ca29957987b389d13b41ad8c30bdda16a
BearSized <Styles@Bearsized.com>
2025-05-30 14:14:24 -0400

    Update googleDriveHelpers.js

--- services/googleDriveHelpers.js
+++ services/googleDriveHelpers.js
@@ -25,3 +25,24 @@ async function moveFileToFolder(fileId, newParentId) {
 }
 
 module.exports = { moveFileToFolder };
+
+const { google } = require("googleapis");
+const auth = require("./auth"); // Adjust if auth is handled differently
+
+async function moveFileToFolder(fileId, folderId) {
+  const drive = google.drive({ version: "v3", auth });
+
+  const file = await drive.files.get({ fileId, fields: "parents" });
+  const previousParents = file.data.parents?.join(",") || "";
+
+  const response = await drive.files.update({
+    fileId,
+    addParents: folderId,
+    removeParents: previousParents,
+    fields: "id, parents",
+  });
+
+  return response.data;
+}
+
+module.exports = { moveFileToFolder, ...otherExports };
